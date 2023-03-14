ce2a9a5669bf02a128de75acda6e39e35db22f3a
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2023-03-14 12:50:27 +0000

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,12 +5,11 @@ const value = parseInt(process.argv[2]);
 if (isNaN(value)) {
   console.log('Missing size');
 } else if (value > 0) {
-  let string = '';
   for (let i = 0; i < value; i++) {
+    let string = '';
     for (let j = 0; j < value; j++) {
       string += 'X';
     }
-    string += '\n';
+    console.log(string);
   }
-  console.log(string);
 }
