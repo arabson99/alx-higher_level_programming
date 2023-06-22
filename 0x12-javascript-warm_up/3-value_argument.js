0777b6f65dca0b5b807d6e6d57d6c82d94bc6bd7
arabson99 <111277887+arabson99@users.noreply.github.com>
2023-06-22 19:59:05 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else if (process.argv[1]) {
-  console.log('No argument');
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const i of lang) {
-  console.log(i);
-}
