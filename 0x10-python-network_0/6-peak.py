2c1ec1b2a198cfe4d602ff29e6745e506ac4918d
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-07-05 02:00:28 +0100

    checked

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,10 +6,10 @@ def find_peak(list_of_integers):
 
     if not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers) - 1
+    lo, hi = 0, len(list_of_integers) - 1
     while lo < hi:
         mid = (lo + hi) // 2
+        # Compare middle element with its right neighbor
         if list_of_integers[mid] < list_of_integers[mid + 1]:
             lo = mid + 1
         else:
