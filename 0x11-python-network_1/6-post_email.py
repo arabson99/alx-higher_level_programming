ef6aa9f7b79c52cda9f1a3cafcf67f77c729008b
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-07-06 00:17:26 +0100

    checked

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,14 +4,13 @@
 Usage: ./6-post_email.py <URL> <email>
   - Displays the body of the response.
 """
-
 import sys
-import urllib.request
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-	value = {"email": sys.argv[2]}
+    value = {"email": sys.argv[2]}
 
     r = requests.post(url, data=value)
     print(r.text)
