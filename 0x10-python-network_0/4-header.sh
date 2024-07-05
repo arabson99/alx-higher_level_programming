5c256aa4fd5827bc0fd954709b0ac0d8d54e9f8c
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-07-05 01:27:35 +0100

    checked

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+#!/usr/bin/env bash
+
+# Check if a URL was provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# URL provided as the first argument
+URL="$1"
+
+# Send a GET request with the header X-School-User-Id: 98
+curl -s -H "X-School-User-Id: 98" "$URL"
