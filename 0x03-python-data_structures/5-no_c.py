b59e446bcefdc41575cbee8b135116dd100134a8
arabson99 <111277887+arabson99@users.noreply.github.com>
2024-07-14 00:34:29 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return new_string
+     """ removes all characters c and C from a string.
+        """
+        new_string = ""
+        for char in my_string:
+                if char != 'c' and char != 'C':
+                        new_string += char
+        return new_string
